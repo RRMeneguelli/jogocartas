d414e9611180191f4d43940298d2edc2879e2587
RRMeneguelli <rrmeneguelli@gmail.com>
2019-01-10 21:30:08 -0200

    correção de erros e identação

--- sortearcartas.c
+++ sortearcartas.c
@@ -5,48 +5,47 @@
 #define N 52
 
 typedef struct {
-  char face[3];
-  char naipe[2];
-  int valor;
-  char nome[20];
+    char face[3];
+    char naipe[2];
+    int valor;
+    char nome[20];
 } Tcarta;
 
 int sorteio(int *t){
-    srand(time(NULL));
     *t=rand()%N;
-    printf("%ls\n",t);
 }
 
 
 void main(){
-  Tcarta cr[N]; 
-  FILE *baralho;
-  char op[2];
-  int t;
+    Tcarta cr[N]; 
+    FILE *baralho;
+    char op[2];
+    int t;
 
-  baralho = fopen("baralho.txt","r");
-  if (baralho == NULL){
-    printf("Arquivo nao encontrado");
-    return;
-  }
+    srand((unsigned)time(NULL));
 
-  for(int i=0; i<N; i++) {
-    fscanf(baralho, "%s %s %d %[^\n]s", cr[i].face, cr[i].naipe, &cr[i].valor, cr[i].nome);
-  }
+    baralho = fopen("baralho.txt","r");
+    
+    if (baralho == NULL){
+        printf("Arquivo nao encontrado");
+        return;
+    }
 
-  fclose(baralho);
+    for(int i=0; i<N; i++) {
+        fscanf(baralho, "%s %s %d %[^\n]s", cr[i].face, cr[i].naipe, &cr[i].valor, cr[i].nome);
+    }
+
+    fclose(baralho);
 
 	t=0;
-        printf("Deseja sortear uma carta? (s/n)\n");
-        scanf("%s",op);
+    printf("Deseja sortear uma carta? (s/n)\n");
+    scanf("%s",op);
         
-        while(strcmp(op,"s") == 0){
+    while(strcmp(op,"s") == 0 || strcmp(op,"S") == 0){
         sorteio(&t);
         printf("O numero sorteado foi: %d. \n",t);
         printf("A carta sorteada foi: %s. \n",cr[t-1].nome);
         printf("Deseja sortear outra carta? (s/n)\n");
         scanf("%s",op);
-	}
-  
-
+    }
 }
